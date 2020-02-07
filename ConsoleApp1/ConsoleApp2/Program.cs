f27ed5c2833f528fb6e52b41e0f5e36180ef27c9
DarkJkeee <46789078+DarkJkeee@users.noreply.github.com>
2020-02-07 23:31:38 +0300

    Revert "Add files via upload"

--- ConsoleApp1/ConsoleApp2/Program.cs
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -1,59 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.IO;
 
 namespace ConsoleApp2
 {
-    // Объявление делегата MathOperation, который хранит ссылку на метод к соответствующей операции.
-    delegate double MathOperation(double a, double b);
-    // Объявлене делегата-типа ErrorNotificationType.
-    delegate void ErrorNotificationType(string message);
-
     class Program
     {
+        // Объявление делегата MathOperation, который хранит ссылку на метод к соответствующей операции.
+        delegate double MathOperation(double a, double b);
+        // Объявление словаря для определения операции.
+        static readonly Dictionary<string, MathOperation> operations = new Dictionary<string, MathOperation>();
+
         static void Main()
         {
             // Счетчик для Error.
             int k = 0;
-            // Очищаем файл перед записью.
-            File.WriteAllText("answers.txt", "");
+            // Заполнение словаря методами.
+            operations.Add("+", (a, b) => a + b);
+            operations.Add("-", (a, b) => a - b);
+            operations.Add("*", (a, b) => a * b);
+            operations.Add("/", (a, b) => a / b);
+            operations.Add("^", (a, b) => Math.Pow(a, b));
 
-            // Подписываем обработчики на событие.
-            Calculator.Handler(ConsoleErrorHandler);
-            Calculator.Handler(ResultErrorHandler);
+            string[] res = Calculation();
+            
+            File.WriteAllLines("results.txt", CalculationChecker(res, ref k));
 
-            // Записываем результат сравнения в файл.
-            File.WriteAllLines("results.txt", CalculationChecker(Calculation(), ref k));
             File.AppendAllText("results.txt", k.ToString());
-            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Метод, который разбивает выражение и вычисляет его.
+        /// </summary>
+        /// <param name="expr"></param>
+        /// <returns></returns>
+        static double Calculate(string expr)
+        {
+            double num1 = 0, num2 = 0;
+            string number1 = "", number2 = "";
+            string operation = "";
+            int size = expr.Length;
+
+            for (int i = 0; i < size; ++i)
+            {
+                if (expr[i] != ' ')
+                {
+                    number1 += expr[i];
+                }
+                else
+                {
+                    size = i;
+                    operation += expr[i + 1];
+
+                    for (int j = i + 2; j < expr.Length; ++j)
+                    {
+                        number2 += expr[j];
+                    }
+                }
+            }
+            try
+            {
+                num1 = double.Parse(number1);
+                num2 = double.Parse(number2);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат строки!");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Пустая строка!");
+            }
+
+
+            return operations[operation](num1, num2);
         }
 
         /// <summary>
         /// Метод, который считывает выражения из файла и записывает результат в новый файл.
         /// </summary>
         /// <returns></returns>
-        static public string[] Calculation()
+        static string[] Calculation()
         {
             string[] str = File.ReadAllLines("expressions.txt");
             string[] res = new string[str.Length];
-
             for (int i = 0; i < str.Length; ++i)
             {
-                try
-                {
-                    double num = Calculator.Calculate(str[i]);
-                    res[i] = $"{Math.Round(num, 3, MidpointRounding.ToEven):f3}";
-                    File.AppendAllText("answers.txt", $"{res[i]}\r\n");
-                }
-                catch(OverflowException)
-                {
-                    res[i] = double.PositiveInfinity.ToString();
-                }
-                catch(Exception ex)
-                {
-                    res[i] = ex.Message;
-                }
+                double num = Calculate(str[i]);
+                // Форматируем num.
+                res[i] = String.Format("{0:f3}", num);
+
             }
+            File.WriteAllLines("answers.txt", res);
             return res;
         }
 
@@ -69,9 +110,9 @@ namespace ConsoleApp2
             string[] checker = new string[str.Length];
             for (int i = 0; i < str.Length; ++i)
             {
-                if (str[i] == res[i])
+                if(str[i] == res[i])
                 {
-                    checker[i] = "OK";
+                    checker[i] = "OK"; 
                 }
                 else
                 {
@@ -80,77 +121,6 @@ namespace ConsoleApp2
                 }
             }
             return checker;
-        }
-        /// <summary>
-        /// Обработчик события, который выводит на консоль сообщение об ошибке.
-        /// </summary>
-        /// <param name="message"></param>
-        public static void ConsoleErrorHandler(string message)
-        {
-            Console.WriteLine(message + " " + DateTime.Now);
-        }
-        /// <summary>
-        /// Обработчик события, который записывает ошибку в файл.
-        /// </summary>
-        /// <param name="message"></param>
-        public static void ResultErrorHandler(string message)
-        {
-            File.AppendAllText("answers.txt", message + "\r\n");
-        }
-    }
-
-    class Calculator
-    {
-        static event ErrorNotificationType ErrorNotification;
-        // Словарь для хранения математический операций.
-        static Dictionary<string, MathOperation> operations = new Dictionary<string, MathOperation>()
-        {
-            ["+"] = (a, b) => a + b,
-            ["-"] = (a, b) => a - b,
-            ["*"] = (a, b) => a * b,
-            ["/"] = (a, b) => a / b,
-            ["^"] = (a, b) => Math.Pow(a, b),
-        };
-        public static void Handler(ErrorNotificationType e)
-        {
-            ErrorNotification += e;
-        }
-
-        /// <summary>
-        /// Метод, который разбивает выражение и возвращает результат вычисления.
-        /// </summary>
-        /// <param name="expr"></param>
-        /// <returns></returns>
-        static public double Calculate(string expr)
-        {
-            double ans = 0;
-            string[] numbers = expr.Split(' ');
-            try
-            {
-                double num1 = double.Parse(numbers[0]);
-                double num2 = double.Parse(numbers[2]);
-                string operation = numbers[1];
-                if (num2 == 0 && operation == "/")
-                {
-                    throw new DivideByZeroException("bruh");
-                }
-                if (!operations.ContainsKey(operation))
-                {
-                    throw new KeyNotFoundException("неверный оператор");
-                }
-                ans = operations[operation](num1, num2);
-                return ans;
-            }
-            catch(OverflowException)
-            {
-                ErrorNotification(double.PositiveInfinity.ToString());
-                throw;
-            }
-            catch (Exception ex)
-            {
-                ErrorNotification(ex.Message);
-                throw;
-            }
         }
     }
 }
